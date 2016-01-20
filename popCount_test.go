821d3ab7d995251838009454c9b536897a9d1192
Jim Dixon <jddixon@gmail.com>
2016-01-19 17:04:11 -0800

    dropped use of intmath/intgr

--- popCount_test.go
+++ popCount_test.go
@@ -3,7 +3,8 @@ package xlUtil_go
 // popCount_test.go
 
 import (
-	"code.google.com/p/intmath/intgr"
+	// has disappeared
+	// "code.google.com/p/intmath/intgr"
 	"fmt"
 	xr "github.com/jddixon/rnglib_go"
 	. "gopkg.in/check.v1"
@@ -35,9 +36,9 @@ func (s *XLSuite) TestSWAR32(c *C) {
 	for i := 0; i < 8; i++ {
 		n := uint32(rng.Int63())
 		slowCount := slowBitCount32(n)
-		intgrCount := uint(intgr.BitCount(int(n)))
+		//intgrCount := uint(intgr.BitCount(int(n)))
 		swar32Count := BitCount32(n)
-		c.Assert(swar32Count, Equals, intgrCount)
+		//c.Assert(swar32Count, Equals, intgrCount)
 		c.Assert(swar32Count, Equals, slowCount)
 	}
 }
@@ -50,9 +51,9 @@ func (s *XLSuite) TestSWAR64(c *C) {
 		n2 := uint64(rng.Int63())
 		n := (n1 << 32) ^ n2 // we want a full 64 random bits
 		slowCount := slowBitCount64(n)
-		intgrCount := uint(intgr.BitCount(int(n)))
+		// intgrCount := uint(intgr.BitCount(int(n)))
 		swar64Count := BitCount64(n)
-		c.Assert(swar64Count, Equals, intgrCount)
+		//c.Assert(swar64Count, Equals, intgrCount)
 		c.Assert(swar64Count, Equals, slowCount)
 	}
 }
